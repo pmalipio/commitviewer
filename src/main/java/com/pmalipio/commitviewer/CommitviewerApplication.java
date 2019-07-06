7078b513571802f0522fb8de8db275a7e6ff4885
Pedro Alípio <pedro.alipio@gmail.com>
2019-07-06 21:31:18 +0100

    Initial commit

---
+++ src/main/java/com/pmalipio/commitviewer/CommitviewerApplication.java
@@ -0,0 +1,13 @@
+package com.pmalipio.commitviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitviewerApplication.class, args);
+    }
+
+}
